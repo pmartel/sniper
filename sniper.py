685c01affde2926eb416ba3aa4c2803c9d5c52d4
Phil Martel <pmartel@alum.mit.edu>
2014-10-30 21:59:54 -0400

    in process.  Added a variable center array that works is the case that
    matches the original center.  Not general if we're at an edge.  Need
    to fix resistor()

--- sniper.py
+++ sniper.py
@@ -5,7 +5,19 @@ method and print the result """
 
 from TwoD import *
 
+class Center:
+    """set up a variable center array (store the info)"""
+    def __init__( self, rSize, cSize, r1,c1,r0,c0):
+        self.rSize = rSize
+        self.cSize = cSize
+        self.r1 = r1
+        self.c1 = c1
+        self.r0 = r0
+        self.c0 = c0
+        
 def relax(a):
+    """run relaxation on the array a.  Each point becomes the average of
+       the 4 adjacent points """
     rn = a.rows
     cn = a.cols
     new = TwoD(rn,cn)
@@ -13,7 +25,7 @@ def relax(a):
     for r in range(rn):
         for c in range(cn):
             new.arr[c][r] = a.arr[c][r]
-    # relax  this takes care of edges, but is messy
+    # this takes care of edges, but is messy
     for r in range(rn):
         if r == 0:
             for c in range(cn):
@@ -49,18 +61,15 @@ def relax(a):
                     a.arr[c][r] = (new.arr[c][r-1] + new.arr[c][r+1] +
                                    new.arr[c-1][r] + new.arr[c+1][r])/4
             
-            
-    reInit(a)
-#    a.display()
     
     
-def reInit(a):
-    b=int((a.rows-1)/2)
-    a.set(b,b+2,1)
-    a.set(b+1,b,0)
+def reInit(a,center):
+    b=int((a.rows-center.rSize)/2)
+    a.set(b+center.r1,b+center.c1,1)
+    a.set(b+center.r0,b+center.c0,0)
 
-def resistor(a,b):
-    # a.arr[b+2][b] == 1
+def resistor(a,center):
+    b=int((a.rows-center.rSize)/2)
     if b == 0:
         current = (1-a.arr[1][0]) + (1-a.arr[2][1])
     else:
@@ -71,20 +80,29 @@ def resistor(a,b):
 
 
 #main
+center = Center(2, 3, 0, 2, 1,0)
+#center = Center(2, 2, 0, 1, 1,0)
 b = int(input('blanks around center? '))
-arr = TwoD(2*b+2,2*b+3,0)
+arr = TwoD(2*b+center.rSize,2*b+center.cSize,0)
 
 # top of loop
-reInit(arr)
+reInit(arr,center)
 
 #arr.display()
 
 again = 'y'
 while again != 'n':
+    # it takes 2 relaxation steps to change the resistance
     relax(arr)
-    r = resistor(arr,b)
-    print('resistance=',r)
+    reInit(arr,center)
+    relax(arr)
+    reInit(arr,center)
+    #arr.display()
+    r = resistor(arr,center)
+    print('resistance=',round(r,6))
     again = input('again?(y/n)')
+arr.display()
+
 
 
 
